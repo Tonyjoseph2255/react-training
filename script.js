9886d9b712cc279162821240c89ac12a256bb2c4
Tonyjoseph2255 <101250944+Tonyjoseph2255@users.noreply.github.com>
2023-02-16 16:29:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("registration-form").addEventListener("submit",function(event){
+    event.preventDefault();
+    // let username = document.getElementById("username").value;
+    // let firstName= document.getElementById("firstname").value;
+    // let lastName=document.getElementById("lastname").value;
+    // let dob=document.getElementById("dob").value;
+    // let gender=document.getElementById("gender").value;
+    // let password= document.getElementById("password").value;
+    // let confirm_password= document.getElementById("confirm-password").value;
+    
+    window.location.href="table.html";
+    
+    
+});
